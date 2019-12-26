31dc521ec53ff994e6f35a8dcc419adb56bf53d4
Anton Skrichevsky <skrtoha@gmail.com>
2019-12-26 11:38:04 +0200

    задается количество и цена при возврате

--- admin/js/orders.js
+++ admin/js/orders.js
@@ -229,7 +229,7 @@ $(function(){
 			data: data,
 			success: function(response){
 				// console.log(response); return false;
-				// document.location.reload();
+				document.location.reload();
 			}
 		})
 	})

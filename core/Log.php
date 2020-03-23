3059dfee985b929f1772e0f445cf4c61ca1d6198
Anton Skrichevsky <skrtoha@gmail.com>
2020-03-23 16:15:08 +0200

    В глобальный лог добавлен метод автоматического логорования при наличии исключения

--- core/Log.php
+++ core/Log.php
@@ -1,11 +1,25 @@
 <?php
 namespace core;
 class Log{
-	public static function insert($params, $flag){
+	public static function insert($params, $flag = null){
 		return $GLOBALS['db']->insert(
 			'logs',
 			$params,
 			$flag
 		);
+	}
+	/**
+	 * insert into log, if there is an Exception
+	 * @param  Exception $e 
+	 * @param  array $params 'text' - requeired, 'query' - optional
+	 * @return boolean true if inserted successfully
+	 */
+	public static function insertThroughException($e, $params = []){
+		return self::insert([
+			'url' => $_SERVER['REQUEST_URI'],
+			'query' => isset($params['query']) ? $params['query'] : '',
+			'trace' => $e->getTraceAsString(),
+			'text' => $e->getMessage()
+		]);
 	}
 }

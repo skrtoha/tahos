b65ae09eca9a2564d209894f486f4dfab129bd07
skrtoha <git25655>
2020-04-01 09:30:16 +0300

    Тестирование settings

--- tests/SettingTest.php
+++ tests/SettingTest.php
@@ -1,8 +1,27 @@
 <?php
 class SettingTest extends \PHPUnit\Framework\TestCase{
-    public function testSetting(){
-        $db = new core\DataBase();
-        $row = $db->select_one('settings', '*', "`id`=1");
-        $this->assertNotEmpty($row, 'ошибка получения settings');
+	public function setUp(): void
+	{
+		$this->db = new core\DataBase();
+	}
+	/**
+	 * @dataProvider getSettings
+	 */
+    public function testSetting($settings){
+        $this->assertNotEmpty($settings);
+        foreach($settings as $key => $value) return $value;
+    }
+    /**
+     * @depends testSetting
+     */
+    public function testIdSetting($id){
+    	$this->assertNotEquals(1, $id);
+    }
+    public function getSettings(){
+    	$db = new core\DataBase();
+    	$settings = $db->select('settings', '*', "`id` = 1");
+    	$output = [];
+    	foreach($settings as $key => $value) $output[] = [$key => $value];
+    	return $output;
     }
 }

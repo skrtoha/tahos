7e596f74a58f298c5167ac18a0135ea6b2695488
skrtoha <31104555+skrtoha@users.noreply.github.com>
2020-09-28 14:12:46 +0300

    Интуитивный поиск. Если есть форма, то при нажатии Ввода или Submit отрабатывается событие формы по умолчанию.

--- vendor/intuitive_search/script.js
+++ vendor/intuitive_search/script.js
@@ -77,7 +77,10 @@
 			})
 			$(document).on('click', function(e){
 				let target = $(e.target);
-				if(target.attr('type') == 'submit') target.closest('form').submit();
+				if(target.attr('type') == 'submit'){
+					target.closest('form').submit();
+					return false;
+				} 
 				let mainNumber = $(e.target).closest('.intuitiveSearch_wrap').find('input.intuitive_search').attr('intuitiveSearch_number');
 				if (mainNumber == 'undefined'){
 					$(document).find('.searchResult_list').hide();

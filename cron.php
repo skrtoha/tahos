362bec78db221afaf84abb73fd94549260e5b91f
skrtoha <skrtoha@gmail.com>
2021-06-01 21:12:31 +0300

    В cron.php добавлен updateCurrencies

--- cron.php
+++ cron.php
@@ -61,5 +61,41 @@ switch ($_SERVER['argv'][1]){
         $logger->alert('Отправка заказа в Армтек');
         core\Provider\Armtek::sendOrder();
         break;
+    case 'updateCurrencies':
+        $logger->alert('Обновление валют');
+        $date = date("d/m/Y");
+        $link = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=$date";
+        $content = file_get_contents($link);
+        $dom = new domDocument("1.0", "cp1251");
+        $dom->loadXML($content);
+        $root = $dom->documentElement;
+        $childs = $root->childNodes;
+        $data = array('USD', 'EUR', 'JPY', 'CNY', 'UAH');
+        for ($i = 0; $i < $childs->length; $i++) {
+            $childs_new = $childs->item($i)->childNodes;
+            for ($j = 0; $j < $childs_new->length; $j++) {
+                $el = $childs_new->item($j);
+                $code = $el->nodeValue;
+                if (in_array($code, $data)) $data[] = $childs_new;
+            }
+        }
+        for ($i = 0; $i < count($data); $i++) {
+            $list = $data[$i];
+            for ($j = 0; $j < $list->length; $j++) {
+                $el = $list->item($j);
+                if ($el->nodeName == "CharCode") $charcode = $el->nodeValue;
+                elseif ($el->nodeName == "Value") $value = $el->nodeValue;
+                if ($value and $charcode) $currencies[$charcode] = str_replace(',', '.', $value);
+            }
+        }
+        foreach ($currencies as $charcode => $value){
+            switch($charcode){
+                case 'UAH': $val = $value/10; break;
+                case 'JPY': $val = $value / 100; break;
+                default: $val = $value;
+            }
+            $db->update('currencies', array('rate' => $val), "`charcode`='$charcode'");
+        }
+        break;
 }
 $logger->alert('Обработка '.$_SERVER['argv'][1].' закончена');

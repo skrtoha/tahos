e59c43d437c5eef87b65c0010dd63c62fbc291cb
a.skrichevskij <a.skrichevskij@mebot24.ru>
2022-11-19 21:03:48 +0300

    Пользователи в рассылке прайсов в админке

--- admin/templates/subscribePrices.php
+++ admin/templates/subscribePrices.php
@@ -29,19 +29,58 @@ switch($_GET['act']){
 		break;
 	default:
 		$status .= "Рассылка прайсов";
-		$where = '';
+		$where_u = 'u.is_subscribe = 1 AND ';
+        $where_sp = '1 AND ';
 		$params['search'] = '';
-		if (isset($_GET['search'])){
+		if (isset($_GET['search']) && $_GET['search']){
 			$params['search'] = $_GET['search'];
-			$where = "email LIKE '%{$_GET['search']}%' OR title LIKE '%{$_GET['search']}%' OR phone LIKE '%{$_GET['search']}%'";
+			$string = "email LIKE '%{$_GET['search']}%' OR title LIKE '%{$_GET['search']}%' OR phone LIKE '%{$_GET['search']}%' AND ";
+            $where_u .= $string;
+            $where_sp .= $string;
 		}
-		$params['all'] = $db->getCount('subscribe_prices', $where);
+
+        $where_sp = substr($where_sp, 0, -5);
+        $where_u = substr($where_u, 0, -5);
+        $db->query("SET sql_mode = ''");
+
+        $resAll = $db->query("
+            SELECT 
+			    email,
+                title,
+                phone,
+                count(*) as cnt
+            FROM 
+                tahos_subscribe_prices
+            WHERE $where_sp
+            
+            UNION
+            
+            SELECT
+                email,
+                IF(
+                   u.organization_name <> '',
+                   CONCAT_WS(' ', ot.title, u.organization_name),
+                   CONCAT_WS(' ', u.name_1, u.name_2, u.name_3)
+                ) as title,
+                phone,
+                count(*) as cnt
+            FROM tahos_users u
+            LEFT JOIN tahos_organizations_types ot ON ot.id = u.organization_type
+            WHERE $where_u 
+        ");
+        $params['all'] = 0;
+        foreach($resAll as $row) $params['all'] += $row['cnt'];
 		$params['perPage'] = 30;
 		$linkLimit = 10;
 		$params['page'] = $_GET['page'] ?: 1;
 		$params['chunk'] = getChank($params['all'], $params['perPage'], $linkLimit, $params['page']);
 		$start = $params['chunk'][$params['page']] ?: 0;
-		if ($where) $where = "WHERE $where";
+        $params['dir'] = $_GET['dir'] ?? 'ASC';
+
+        $orderBy = '';
+        if (isset($_GET['sort']) && $_GET['sort']){
+            $orderBy = "ORDER BY {$_GET['sort']} {$params['dir']}";
+        }
 		$res_common_list = $db->query("
 			SELECT 
 			    '' as id,
@@ -51,6 +90,7 @@ switch($_GET['act']){
                 '' as date
             FROM 
                 tahos_subscribe_prices
+            WHERE $where_sp
             
             UNION
             
@@ -66,10 +106,15 @@ switch($_GET['act']){
                 (select created from tahos_orders where user_id = u.id order by created desc limit 1) as date
             FROM tahos_users u
             LEFT JOIN tahos_organizations_types ot ON ot.id = u.organization_type
-            WHERE u.is_subscribe = 1
-            order by `date` DESC
+            WHERE $where_u 
+            $orderBy
             LIMIT $start, {$params['perPage']}
 		", '');
+
+        $params['rotated'] = $params['dir'] == 'DESC' ? 'rotated' : '';
+        if ($params['dir'] == 'DESC') $params['dir'] = 'ASC';
+        else $params['dir'] = 'DESC';
+
 		commonList($res_common_list, $params);
 }
 function commonList($res_common_list, $params){?>
@@ -83,10 +128,31 @@ function commonList($res_common_list, $params){?>
 	</div>
 	<table class="t_table" cellspacing="1">
 		<tr class="head">
-			<td>Email</td>
-			<td>Название</td>
+			<td>
+                <a href="/admin/?view=subscribePrices&sort=email&search=<?=$params['search']?>&dir=<?=$params['dir']?>">
+                    Email
+                </a>
+                <?if (isset($_GET['sort']) && $_GET['sort'] == 'email'){?>
+                    <span class="icon-arrow-up2 <?=$params['rotated']?>"></span>
+                <?}?>
+            </td>
+			<td>
+                <a href="/admin/?view=subscribePrices&sort=title&search=<?=$params['search']?>&dir=<?=$params['dir']?>">
+                    Название
+                </a>
+                <?if (isset($_GET['sort']) && $_GET['sort'] == 'title'){?>
+                    <span class="icon-arrow-up2 <?=$params['rotated']?>"></span>
+                <?}?>
+            </td>
 			<td>Телефон</td>
-			<td>Дата заказа</td>
+			<td>
+                <a href="/admin/?view=subscribePrices&sort=date&search=<?=$params['search']?>&dir=<?=$params['dir']?>">
+                    Дата заказа
+                </a>
+                <?if (isset($_GET['sort']) && $_GET['sort'] == 'date'){?>
+                    <span class="icon-arrow-up2 <?=$params['rotated']?>"></span>
+                <?}?>
+            </td>
 			<td></td>
 		</tr>
 		<?if ($res_common_list->num_rows){
@@ -95,7 +161,7 @@ function commonList($res_common_list, $params){?>
 					<td label="Email"><?=$item['email']?></td>
 					<td label="Название"><?=$item['title']?></td>
 					<td label="Телефон"><?=$item['phone']?></td>
-					<td label="Телефон"><?=$item['date']?></td>
+					<td label="Дата"><?=$item['date']?></td>
 					<td label="">
 						<a tooltip="Удалить" class="delete" href="?view=subscribePrices&act=delete&email=<?=$item['email']?>&user_id=<?=$item['id']?>">
 							<span class="icon-cancel-circle1"></span>

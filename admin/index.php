2eacc7c5ed7f8390565645d1160b138572982933
skrtoha <git25655>
2023-03-29 11:21:27 +0300

    Initial

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,4 @@
 <?php
-/*$memcache = new Memcache();
-$memcache->connect('localhost', 11211);*/
-phpinfo();
 ini_set('error_reporting', E_PARSE | E_ERROR);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);

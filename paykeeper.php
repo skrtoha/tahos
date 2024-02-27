ac3476f1c6bf993459105586e9740232680bcc75
Anton <skrtoha@gmail.com>
2024-02-27 21:55:36 +0300

    Реализована возможность оплаты заказа

--- paykeeper.php
+++ paykeeper.php
@@ -1,4 +1,10 @@
 <?php
+if (isset($_GET['fail']) && $_GET['fail'] == 'true'){
+    require_once ("{$_SERVER['DOCUMENT_ROOT']}/core/functions.php");
+    message('Оплата не прошла', false);
+    header("Location: /account");
+    die();
+}
 require_once ("{$_SERVER['DOCUMENT_ROOT']}/core/Database.php");
 \core\Messengers\Telegram::writeLogFile($_REQUEST);
 \core\Payment\Paykeeper::setPayment($_REQUEST);
